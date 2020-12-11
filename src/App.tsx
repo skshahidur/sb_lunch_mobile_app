3dc6cfe704eaad29627c0a5057eb1fee0cd29998
skshahidur <skshahidur@gmail.com>
2020-12-11 23:37:52 +0100

    all assets

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,15 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+// Import pages
+import SplashScreen from "./pages/SplashScreen/SplashScreen";
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact={true} />
+        {/* <Route path="/splashscreen" component={SplashScreen} exact={true} /> */}
         <Route exact path="/" render={() => <Redirect to="/home" />} />
       </IonRouterOutlet>
     </IonReactRouter>
